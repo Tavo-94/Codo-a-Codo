08770e04325387da53a3b848510731329cf98e37
Tavo-94 <gustavo.mph@gmail.com>
2022-10-24 04:01:12 -0300

    agregue tp js

---
+++ Proyectos CaC/Actividad_Obligatoria_JS/js/script.js
@@ -0,0 +1,23 @@
+function calcularTotal(){
+    var cantidad = document.getElementById("input-cantidad").value;
+    var categoria = document.getElementById("categoria");
+    var textoCategoria = categoria.options[categoria.selectedIndex].text;
+    var precioBase = 200;
+    var total = 0;
+
+    if (textoCategoria == "Estudiante") {
+        total = precioBase * cantidad * 0.2;
+    } else if (textoCategoria == "Trainee") {
+        total = precioBase * cantidad * 0.5;
+    } else if (textoCategoria == "Junior") {
+        total = precioBase * cantidad * 0.85;
+    } else {
+        total = precioBase * cantidad;
+    }
+
+    document.getElementById("totalAPagar").innerHTML = "Total a Pagar: $" + total;
+}
+
+function borrarTotal(){
+    document.getElementById("totalAPagar").innerHTML = "Total a Pagar: $";
+}
